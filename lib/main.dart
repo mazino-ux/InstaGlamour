9411c54dd70bdd4667eb677ac0ce02b0f3500ba6
MathenaDev <mazinoishioma@gmail.com>
2025-02-25 09:14:33 +0100

    Addition

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/home.dart';
+import 'package:instagram_clone/pages/addPost.dart';
+import 'package:instagram_clone/pages/explore.dart';
+import 'package:instagram_clone/pages/home.dart';
+import 'package:instagram_clone/pages/reels.dart';
+import 'package:instagram_clone/pages/profile.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,13 +15,22 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Home(),
-          // child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      routes: {
+        "/home": (context) => Home(),
+        "/explore": (context) => Explore(),
+        "/addPost": (context) => const AddPost(),
+        "/reels": (context) => const Reels(),
+        "/profile": (context) => const Profile(),
+      },
+      // home: Scaffold(
+      //   body: Center(
+      //     child: Home(),
+      //     // child: Text('Hello World!'),
 
-        ),
-      ),
+      //   ),
+      // ),
     );
   }
 }
