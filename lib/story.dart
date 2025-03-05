e8878ce3bddb722d3a9f9e8acca535e91538351a
MathenaDev <mazinoishioma@gmail.com>
2025-03-05 21:44:44 +0100

    update/bottom-dart

--- lib/story.dart
+++ lib/story.dart
@@ -11,6 +11,7 @@ class Story extends StatelessWidget {
         height: 80,
         width: 80,
         decoration: const BoxDecoration(
+          // border: Border(),
           color: Color.fromARGB(255, 106, 9, 124),
           shape: BoxShape.circle
         ),
